1c4f3d5f27cf21b808d4a04fbdb34a86bd14d4b1
zodakzach <zachary.cervenka@gmail.com>
2024-04-17 00:17:01 -0400

    Added avg cost index

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import ApexCharts from 'apexcharts';
 import Papa from 'papaparse';
 
 var chart = null;
+var avgChart = null;
 
 // Function to fetch and plot minimum wage and cost of living data
 async function plotMinWageAndCostOfLiving(location) {
@@ -38,20 +39,6 @@ async function plotMinWageAndCostOfLiving(location) {
     //remove nan
     minWageYears.pop()
 
-    // Function to interpolate missing values
-    function interpolateMissingValues(data) {
-        let prevValue = 0; // Default previous value
-        for (let i = 0; i < data.length; i++) {
-            if (!isNaN(parseFloat(data[i]))) { // Check if the value is not missing
-                prevValue = parseFloat(data[i]); // Update previous value
-                data[i] = prevValue
-            } else {
-                data[i] = prevValue; // Replace missing value with previous value
-            }
-        }
-        return data;
-    }
-
     // Apply interpolation to the minimum wage values, replacing missing values with previous value or 0
     const minWageValues = interpolateMissingValues(Object.values(minWageLocationData[0]));
     // Remove nan
@@ -72,7 +59,7 @@ async function plotMinWageAndCostOfLiving(location) {
     const chartOptions = {
         chart: {
             type: 'line',
-            height: 400,
+            height: 'auto',
             background: '#374151'
         },
         series: [
@@ -145,10 +132,16 @@ async function plotMinWageAndCostOfLiving(location) {
 
                     text: 'Minimum Wage (Hourly $)',
                 },
+                labels: {
+                    formatter: (value) => { return value.toFixed(2) },
+                },
             },
             {
                 seriesName: 'Federal Minimum Wage',
                 show: false,
+                labels: {
+                    formatter: (value) => { return value.toFixed(2) },
+                },
             },
             {
                 seriesName: 'Cost of Living Index',
@@ -156,6 +149,9 @@ async function plotMinWageAndCostOfLiving(location) {
                 title: {
                     seriesName: 'Cost of Living Index',
                     text: 'Cost of Living Index',
+                },
+                labels: {
+                    formatter: (value) => { return value.toFixed(2) },
                 },
             },
         ],
@@ -183,6 +179,20 @@ async function plotMinWageAndCostOfLiving(location) {
     chart.render();
 }
 
+// Function to interpolate missing values
+function interpolateMissingValues(data) {
+    let prevValue = 0; // Default previous value
+    for (let i = 0; i < data.length; i++) {
+        if (!isNaN(parseFloat(data[i]))) { // Check if the value is not missing
+            prevValue = parseFloat(data[i]); // Update previous value
+            data[i] = prevValue
+        } else {
+            data[i] = prevValue; // Replace missing value with previous value
+        }
+    }
+    return data;
+}
+
 // Call the function with the desired location
     // Function to handle button click
 function handlePlotButtonClick() {
@@ -198,3 +208,150 @@ function handlePlotButtonClick() {
 // Add event listener to the button
 var plotButton = document.getElementById("plotButton");
 plotButton.addEventListener("click", handlePlotButtonClick);
+
+
+async function plotFedMinWageAndAvgCostOfLiving(){
+    if (avgChart !== null) {
+        avgChart.destroy();
+    }
+    // Fetch minimum wage data
+    const avgCostIndexResponse = await fetch('public/data/avg_cost_index_data.csv');
+    const avgCostIndexData = await avgCostIndexResponse.text();
+
+    // Fetch minimum wage data
+    const minWageResponse = await fetch('public/data/clean_min_wage_data.csv');
+    const minWageData = await minWageResponse.text();
+
+    // Parse minimum wage CSV data
+    const minWageParsed = Papa.parse(minWageData, { header: true }).data;
+
+    const fedMidWageLocationData = minWageParsed.filter(row => row['State or otherjurisdiction'] === 'Federal ');
+
+    const fedMinWageValues = interpolateMissingValues(Object.values(fedMidWageLocationData[0]));
+    // Remove nan
+    fedMinWageValues.pop()
+
+    // Prepare data for ApexCharts
+    const minWageYears = Object.keys(minWageParsed[0]).map(parseFloat);
+    //remove nan
+    minWageYears.pop()
+
+    const costOfLivingParsed = Papa.parse(avgCostIndexData, { header: true }).data;
+
+    const avgCostLivingYears = costOfLivingParsed.map(row => parseFloat(row['Year']));
+    const avgCostLivingValues = costOfLivingParsed.map(row => parseFloat(row['Avg_Cost_Index']));
+
+    avgCostLivingValues.pop();
+    avgCostLivingYears.pop();
+
+    const minWageYearsAsDates = minWageYears.map(year => new Date(year, 0)); // Months are zero-based, so 0 represents January
+    const costOfLivingYearsAsDates = avgCostLivingYears.map(year => new Date(year, 0)); // Months are zero-based, so 0 represents January
+
+    const chartOptions = {
+        chart: {
+            type: 'line',
+            height: '300',
+            background: '#374151'
+        },
+        series: [
+            {
+                name: 'Federal Minimum Wage',
+                data: fedMinWageValues.map((value, index) => [minWageYearsAsDates[index], value]),
+                dataLabels: {
+                    enabled: true,
+                    formatter: function(value, timestamp) {
+                        return new Date(timestamp).getFullYear(); // Display only the year
+                    }
+                }
+            },
+            {
+                name: 'Cost of Living Index',
+                data: avgCostLivingValues.map((value, index) => [costOfLivingYearsAsDates[index], value]),
+                dataLabels: {
+                    enabled: true,
+                    formatter: function(value, timestamp) {
+                        return new Date(timestamp).getFullYear(); // Display only the year
+                    }
+                }
+            }
+        ],
+
+        grid: {
+            show: false,
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },   
+            yaxis: {
+                lines: {
+                    show: true
+                }
+            },  
+        },
+        xaxis: {
+            type: 'datetime',
+            labels: {
+                datetimeFormatter: {
+                    year: 'yyyy', // Display only the year
+                }
+            },
+            title: {
+                text: 'Year',
+            },
+        },
+        tooltip: {
+            x: {
+                format: 'yyyy', // Format the tooltip to show only the year
+            },
+        },
+        yaxis: [
+            {
+                seriesName: 'Federal Minimum Wage',
+                title: {
+                    seriesName: 'Federal Minimum Wage',
+
+                    text: 'Minimum Wage (Hourly $)',
+                },
+                labels: {
+                    formatter: (value) => { return value.toFixed(2) },
+                },
+            },
+
+            {
+                seriesName: 'Cost of Living Index',
+                opposite: true,
+                title: {
+                    seriesName: 'Cost of Living Index',
+                    text: 'Cost of Living Index',
+                },
+                labels: {
+                    formatter: (value) => { return value.toFixed(2) },
+                },
+            },
+        ],
+        title: {
+            text: `Minimum Wage and Cost of Living Index Over Time in the U.S.`,
+            align: 'center',
+        },
+        legend: {
+            position: 'top',
+        },
+        theme: {
+            mode: 'dark', 
+            palette: 'palette1', 
+            monochrome: {
+                enabled: false,
+                color: '#255aee',
+                shadeTo: 'dark',
+                shadeIntensity: 0.65
+            },
+        }
+    };
+
+    // Render ApexCharts
+    avgChart = new ApexCharts(document.querySelector('#avgChart'), chartOptions);
+    avgChart.render();
+}
+
+plotFedMinWageAndAvgCostOfLiving();
